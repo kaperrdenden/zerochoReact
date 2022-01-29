4aae6fddc6f8ff563b2cabed632efa8fb04f1ad3
kaperrdenden <weather2sogood@gmail.com>
2022-01-29 23:59:35 +0900

    entry, output 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
+const path = require('path');
+
+
 module.exports = {
     name: 'wordrelay-setting',
     mode: 'development', //실서비스 : production
     devtool: 'eval',
     
     entry: {
+        app: ['./client.jsx','WordRelay.jsx'],
         
     }, // 입력
     output:{
-        
+        path: path.join(__dirname, 'dist'), //현재폴더안의 dist를 지정
+        filename: 'app.js'
     }, //출력
 }
