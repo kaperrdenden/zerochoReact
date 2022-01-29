89bf8cdd69110b92e2229266f26237e19b90af59
kaperrdenden <weather2sogood@gmail.com>
2022-01-29 23:55:42 +0900

    webpack.config 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-
+    name: 'wordrelay-setting',
+    mode: 'development', //실서비스 : production
+    devtool: 'eval',
+    
+    entry: {
+        
+    }, // 입력
+    output:{
+        
+    }, //출력
 }
