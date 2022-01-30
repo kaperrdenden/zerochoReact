cec6d6ed2f0071d867d71532d2f7162e4c90780d
kaperrdenden <weather2sogood@gmail.com>
2022-01-30 23:57:48 +0900

    debug:true 추가

--- 구구단/webpack.config.js
+++ 구구단/webpack.config.js
@@ -17,10 +17,12 @@ module.exports ={
                 presets:[
                     ['@babel/preset-env',{
                         targets:{
-                            browsers:['last 2 chrome versions'],
+                            browsers:['> 5% in KR', 'last 2 chrome versions'],
+                            // 오직 크롬의 마지막, 마지막-1 버전만 호환되게
                         },
                         // preset env가 자동으로 옛 브라우저들을 지원해주는데
                         //좀더 구체적으로 써줄 수가 있다.
+                        debug: true, // 개발용에서는 이거 사용
                     }],
                     '@babel/preset-react'],
                 plugins: [],
