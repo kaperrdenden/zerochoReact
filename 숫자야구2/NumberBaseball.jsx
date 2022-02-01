f49920098cdf6365e6571304ed726c43f4d877d0
kaperrdenden <weather2sogood@gmail.com>
2022-02-02 00:17:07 +0900

    NumberBaseball map구현

--- 숫자야구2/NumberBaseball.jsx
+++ 숫자야구2/NumberBaseball.jsx
@@ -2,12 +2,42 @@ const React = require('react');
 const {Component} = React;
 
 
-class NumberBaseball extends React.Component{
+function getNumbers() {
 
+}
+class NumberBaseball extends React.Component{
+    state = {
+        result: '',
+        value: '',
+        answer: getNumbers(),
+        tries: [],
+    }
+    onSubmitForm = () => {
+
+    }
+    onChangeInput = () => {
+
+    }
     render(){
        return(
            <>
-                dasdad
+               <h1>{this.state.result}</h1>
+               <form onSubmit={this.onSubmitForm}>
+                   <input 
+                        maxLength={4}
+                        value={this.state.value}
+                        onChange={this.onChangeInput}
+
+                   />
+               </form>
+               <div>시도:{this.state.tries}</div>
+               <ul>
+                {['사과','바나나','복숭아'].map((v) => {
+                    return(
+                        <li>{v}</li>
+                    )
+                })}  
+               </ul>
            </>
        )
         
