f4415df69f09c1c271e5b6bc50a546fae51388c4
kaperrdenden <weather2sogood@gmail.com>
2022-02-06 22:57:08 +0900

    this.setState -> setState hook으로 바꾸기

--- 숫자야구2/NumberBaseball.jsx
+++ 숫자야구2/NumberBaseball.jsx
@@ -18,28 +18,94 @@ const NumberBaseball = () => {
     const [value, setValue] = useState('');
     const [answer, setAnswer] = useState(getNumbers());
     const [tries, setTries] = useState([]);
+    const onSubmitForm = (e) => {
+        e.preventDefault();  
+        console.log(this.state.value.length);
+        if(this.state.value.length < 4){
+            alert("4자리 수 입력");
+            this.setState({
+                value:'',
+
+            })
+            return;
+        }
+        if(this.state.value === this.state.answer.join('')){
+                setResult('홈런');
+                setTries((prevTries) => {
+                  return  [...prevState.tries, {try: prevState.value, result:'홈런!'}]
+                })
+               
+                alert('게임을 다시 시작합니다');
+                this.setState({
+                    value:'',
+                    answer:getNumbers(),
+                    tries:[],
+
+                });
+        }
+        else{
+            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if (this.state.tries.length >= 9){
+                this.setState({
+                    result:`10번 넘게 틀려서 실패 답은 ${answer.join('')} `, 
+                });
+                alert('게임을 다시 시작합니다');
+                this.setState({
+                    value:'',
+                    answer:getNumbers(),
+                    tries:[],
+
+                });
+            }
+            else {
+                for( let i = 0; i < 4; i += 1){
+                    if(answerArray[i] === this.state.answer[i]){ 
+                        strike += 1;
+                    } else if( this.state.answer.includes(answerArray[i])){
+                        ball += 1;
+                    }
+                }
+            this.setState((prevState) => {
+                return{
+                    tries:[...tprevState.tries, {try: prevState.value, result: `${strike} 스트라이크 ${ball}볼입니다`}],
+                    value:'',
+                }
+              
+            });
+
+            
+        }
+        }
+        console.log(this.state.tries);
+    } 
+    
+    const onChangeInput = (e) => {
+        this.setState({
+            value: e.target.value,
+        });
+
+    }
+ 
     return(
         <>
         <h1>{result}s</h1>
-          <form onSubmit={this.onSubmitForm}>
+          <form onSubmit={onSubmitForm}>
               <input 
                    maxLength={4}
                    value={value}
-                   onChange={this.onChangeInput}
+                   onChange={onChangeInput}
                    value={value}
-
               />
           </form>
           <div>시도:{tries.length}</div>
           <ul>
               {tries.map((v, i) => {
                   console.log(v)
-                  return(
-                   
+                  return(                   
                       <Try  key={i} tryInfo={v} index={i} />
-                      )
-                  
-                  
+                      )        
               })}
           </ul>
        
