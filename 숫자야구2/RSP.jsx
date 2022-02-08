84c85177182ecbea6d3d956593359ebf6f549fbb
kaperrdenden <weather2sogood@gmail.com>
2022-02-08 23:45:17 +0900

    라이프사이클

--- 숫자야구2/RSP.jsx
+++ 숫자야구2/RSP.jsx
@@ -2,7 +2,20 @@ const React = require('react');
 const {Component} = React;
 
 class RSP extends Component{
+    state = {
+        result:'',
+        imgCoord:0,
+        score: 0,
+    }
+    componentDidMount(){ // 비동기 요청을 많이한다
+        setInterval(()=>{
+            console.log("asd");
+        },1000)
+    }
+    componentDidUpdate(){}
+    componentWillUnmount(){}// 비동기요청 정리를 많이한다.
     render(){
+        const {result, score, imgCoord} =this.state;
         return(
             <>
                 <div id="computer" style={{background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0 ` }}></div>
